fd8ce9ef80c00d7a8102af8da8c7dfc6476aab7a
Bianca Milani <107580101+MILANIBIANCA@users.noreply.github.com>
2024-04-11 17:29:08 -0300

    11/04

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo")
 //Laço de repetição
 
+
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
@@ -12,4 +13,17 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+
+
+
+
+
 }
